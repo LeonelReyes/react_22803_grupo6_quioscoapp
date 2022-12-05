e9b467753c21825494665f1cb71d8bc4467f47af
Leonel Isaac Reyes <72421131+LeonelReyes@users.noreply.github.com>
2022-12-05 19:26:25 -0300

    Se consume la api creada y se muestra en el index

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,10 @@
 import Image from "next/image"
+import useQuiosco from "../hooks/useQuiosco"
+import Categoria from "./Categoria";
 
 const Sidebar = () => {
+  const {categorias} = useQuiosco();
+
   return (
     <>
         <Image 
@@ -9,6 +13,18 @@ const Sidebar = () => {
         src= "/assets/img/logo.svg" 
         alt="Imagen logotipo"
         />
+
+        <nav className="mt-10">
+          {categorias.map(categoria => (
+            <Categoria
+            key={categoria.id}
+            categoria = {categoria}
+            />
+
+
+          ))}
+
+        </nav>
 
     </>
   )
--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,14 +1,38 @@
 
 import {useState, useEffect, createContext} from 'react'
+import axios from 'axios'
 
 const QuioscoContext = createContext()
 
 const QuioscoProvider = ({children}) => {
+    const [categorias, setCategorias] = useState ([])
+    const [categoriaActual, setCategoriaActual] = useState ({})
+
+    const obtenerCategorias = async () => {
+        const { data } = await axios('/api/categorias')
+        setCategorias(data)
+    }
+    useEffect(() => {
+        obtenerCategorias()
+    }, [])
+
+    useEffect(() => {
+        setCategoriaActual(categorias[0])
+
+    },[categorias])
+    
+    const handleClickCategoria = id => {
+        const categoria  = categorias.filter(cat => cat.id === id)
+        setCategoriaActual(categoria[0])
+    }
+
 
     return(
         <QuioscoContext.Provider
             value={{
-
+                categorias,
+                categoriaActual,
+                handleClickCategoria
             }}
         >
             {children}
