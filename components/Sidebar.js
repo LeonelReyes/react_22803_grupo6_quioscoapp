dfeb71bbf43fed186a1b38762d3ceb9518345b19
AlbertCandia <kity_lilo@yahoo.com.ar>
2022-12-26 18:03:34 -0300

    cambie la etiqeta <a> por link  de next

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import useQuiosco from '../hooks/useQuiosco';
 import Categoria from './Categoria';
+import Link from 'next/link';
 
 const Sidebar = () => {
   const { categorias } = useQuiosco();
@@ -8,14 +9,14 @@ const Sidebar = () => {
   return (
     <>
       <div className="ml-20">
-        <a href="/">
+        <Link href="/">
           <Image
             width={200}
             height={80}
             src="/assets/img/logo.svg"
             alt="Imagen logotipo"
           />
-        </a>
+        </Link>
       </div>
 
       <nav className="mt-10">
